b22a9d361e238b4a936e5ae908c31b0fa93ee157
Itzmefoxy <68133216+Itzmefoxy@users.noreply.github.com>
2021-09-21 21:36:09 +0700

    Create eval.js

---
+++ Day 3/eval.js
@@ -0,0 +1,28 @@
+const db = require("quick.db")
+//type "npm i quick.db" at shell/console
+module.exports = {
+  name: "eval",
+  ownerOnly: true,
+  category: 'owner',
+  description: 'Only For Owner BRUH -_-',
+  run: async(client, message, args) => {
+      const clean = text => {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+      return text;
+}
+        try {
+ 
+      const code = args.slice().join(" ");
+      let evaled = eval(code);
+ 
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+ 
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+}
+//ownerOnly at config.json
